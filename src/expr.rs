dc35259fd1449477a00bb15ac3eb7fe4259cbc26
Alice McKean <mckean.kylej@gmail.com>
2019-09-12 15:50:49 -0700

    repl it up baby

--- src/expr.rs
+++ src/expr.rs
@@ -80,6 +80,91 @@ pub enum Expr<'a> {
     },
 }
 
+impl<'a> Expr<'a> {
+    pub fn pretty(&self) -> String {
+        fn draw<'a>(expr: &Expr<'a>, lines: &mut Vec<String>, cur: usize) -> usize {
+            use Expr::*;
+            match expr {
+                Var(name) => {
+                    lines.push(format!("{}", name));
+                    cur + 1
+                },
+                Lit(lit) => {
+                    lines.push(format!("{}", lit));
+                    cur + 1
+                },
+                Unary{ operation, child } => {
+                    lines.push(format!("{}", operation));
+                    lines.push("│  ".to_string());
+                    let bottom = draw(child, lines, cur + 2);
+                    lines[cur + 2].insert_str(0, "└──");
+                    for y in cur + 3 .. bottom {
+                        lines[y].insert_str(0, "   ");
+                    }
+                    bottom
+                },
+                Binary{ left, operation, right } => {
+                    lines.push(format!("{}", operation));
+                    lines.push("│  ".to_string());
+                    let top = draw(left, lines, cur + 2);
+                    lines[cur + 2].insert_str(0, "├──");
+                    for y in cur + 3 .. top {
+                        lines[y].insert_str(0, "│  ");
+                    }
+                    lines.push("│  ".to_string());
+                    let bottom = draw(right, lines, top + 1);
+                    lines[top + 1].insert_str(0, "└──");
+                    for y in top + 2 .. bottom {
+                        lines[y].insert_str(0, "   ");
+                    }
+                    bottom
+                },
+                IfThenElse{ condition, if_branch, else_branch } => {
+                    lines.push("if".to_string());
+                    lines.push("│  ".to_string());
+                    let top = draw(condition, lines, cur + 2);
+                    lines[cur + 2].insert_str(0, "├──");
+                    for y in cur + 3 .. top {
+                        lines[y].insert_str(0, "│  ");
+                    }
+                    lines.push("│  ".to_string());
+                    let middle = draw(if_branch, lines, top + 1);
+                    lines[top + 1].insert_str(0, "├──");
+                    for y in top + 2 .. middle {
+                        lines[y].insert_str(0, "│  ");
+                    }
+                    lines.push("│  ".to_string());
+                    let bottom = draw(else_branch, lines, middle + 1);
+                    lines[middle + 1].insert_str(0, "└──");
+                    for y in middle + 2 .. bottom {
+                        lines[y].insert_str(0, "   ");
+                    }
+                    bottom
+                },
+                Let{ name, binder, child } => {
+                    lines.push(format!("let {}=", name));
+                    lines.push("│  ".to_string());
+                    let top = draw(binder, lines, cur + 2);
+                    lines[cur + 2].insert_str(0, "├──");
+                    for y in cur + 3 .. top {
+                        lines[y].insert_str(0, "│  ");
+                    }
+                    lines.push("│  ".to_string());
+                    let bottom = draw(child, lines, top + 1);
+                    lines[top + 1].insert_str(0, "└──");
+                    for y in top + 2 .. bottom {
+                        lines[y].insert_str(0, "   ");
+                    }
+                    bottom
+                }
+            }
+        }
+        let mut lines = vec![];
+        draw(self, &mut lines, 0);
+        lines.join("\n")
+    }
+}
+
 impl<'a> fmt::Display for Expr<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         fn padding(indent: usize) -> String {
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ extern crate combine;
 use combine::{Parser};
 
 use std::thread;
+use std::fs::File;
+use std::io::Write;
+
+use rustyline::{Config, Editor, EditMode};
+use rustyline::error::ReadlineError;
 
 pub mod lexer;
 
@@ -12,28 +17,57 @@ pub mod expr;
 use lexer::{Tokenizer};
 use expr::{expn};
 
-const STACK_SIZE: usize = 100 * 1024 * 1024;
-
-fn run() {
-    let input = "let val x = 2 in if 12 + (1 * 2) mod 3 < x - 2 ** 3 then 1 else 0 end";
-    // use combine::StreamOnce;
-    // let mut tokenizer = Tokenizer::new(input);
-    // while let Ok(t) = tokenizer.uncons() {
-    //     println!("{:?}", t)
-    // };
-    let tokenizer = Tokenizer::new(input);
-    match expn().parse(tokenizer) {
-        Ok((expr, _)) => println!("{}", expr),
-        Err(e) => eprintln!("{}", e)
+pub fn repl() {
+    let prompt = "> ";
+    let config = Config::builder()
+        .edit_mode(EditMode::Emacs)
+        .max_history_size(1000)
+        .history_ignore_space(true)
+        .build();
+    let mut rl = Editor::<()>::with_config(config);
+    let history_file = ".ferus_history";
+    if rl.load_history(&history_file).is_err() {
+        println!("no history file creating[{}]...", history_file);
+        let mut file = File::create(&history_file).unwrap();
+        file.write_all(b"").unwrap();
     }
+    loop {
+        let readline = rl.readline(prompt);
+        match readline {
+            Ok(ref line) if line.is_empty() => {}
+            Ok(ref line) => {
+                let tokenizer = Tokenizer::new(line);
+                match expn().parse(tokenizer) {
+                    Err(err) => {
+                        eprintln!("ERROR: could not parse ({}) because {}", line, err)
+                    },
+                    Ok((expr, _)) => {
+                        rl.add_history_entry(line);
+                        println!("{}", expr.pretty());
+                    }
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                eprintln!("CTRL-C");
+                break
+            }
+            Err(ReadlineError::Eof) => break,
+            Err(err) => {
+                eprintln!("ERROR: {}", err);
+                break
+            }
+        }
+    }
+    rl.save_history(&history_file).unwrap();
 }
+
+const STACK_SIZE: usize = 100 * 1024 * 1024;
 
 fn main() {
     let child = thread::Builder::new()
         .stack_size(STACK_SIZE)
-        .spawn(run)
+        .spawn(repl)
         .unwrap();
-
     child.join().unwrap();
 }
 
